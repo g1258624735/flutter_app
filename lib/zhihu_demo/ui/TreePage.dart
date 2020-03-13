f9a8e65c1471d9e4665ff8e7a5f76812d6f130e6
gxj <1258624735@qq.com>
2020-03-13 17:29:44 +0800

    初始化提交

--- lib/zhihu_demo/ui/TreePage.dart
+++ lib/zhihu_demo/ui/TreePage.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/zhihu_demo/http/api.dart';
+import 'package:flutter_app/zhihu_demo/http/http_util.dart';
 
 //列表页
 class TreePage extends StatefulWidget {
@@ -7,26 +10,39 @@ class TreePage extends StatefulWidget {
 }
 
 class _TreePageState extends State<TreePage> {
+  var listData;
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  ///获取列表数据
+  void _getData() {
+    HttpUtil.get(Api.TREE, (data) {
+      if (data = null) {
+        setState(() {
+          listData = data;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: Text("列表"),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("列表"),
         ),
-      ),
-    );
+        body: ListView.builder(
+            itemBuilder: (context, i) => _buildItem(i),
+            itemCount: listData.length));
+  }
+
+  ///处理listview 每个条目的数据
+  _buildItem(int i) {
+    
+
   }
 }
