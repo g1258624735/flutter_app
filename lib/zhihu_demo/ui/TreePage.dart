6ba3c9d011baebf4fc97bd9c1e4c3f48c91056d9
gxj <1258624735@qq.com>
2020-03-17 18:18:32 +0800

    初始化提交

--- lib/zhihu_demo/ui/TreePage.dart
+++ lib/zhihu_demo/ui/TreePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/test/ToastPostion.dart';
 import 'package:flutter_app/zhihu_demo/http/api.dart';
 import 'package:flutter_app/zhihu_demo/http/http_util.dart';
 
@@ -21,7 +22,7 @@ class _TreePageState extends State<TreePage> {
   ///获取列表数据
   void _getData() {
     HttpUtil.get(Api.TREE, (data) {
-      if (data = null) {
+      if (data != null) {
         setState(() {
           listData = data;
         });
@@ -31,18 +32,49 @@ class _TreePageState extends State<TreePage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(
-          title: Text("列表"),
-        ),
-        body: ListView.builder(
-            itemBuilder: (context, i) => _buildItem(i),
-            itemCount: listData.length));
+    if (listData == null) {
+      return new Scaffold(
+          appBar: AppBar(
+            title: Text("列表"),
+          ),
+          body: Center(child: CircularProgressIndicator()));
+    } else {
+      return new Scaffold(
+          appBar: AppBar(
+            title: Text("列表"),
+          ),
+          body: ListView.builder(
+              itemBuilder: (context, i) => _buildItem(i),
+              itemCount: listData.length));
+    }
   }
 
   ///处理listview 每个条目的数据
-  _buildItem(int i) {
-    
-
+  Widget _buildItem(int i) {
+    var data = listData[i];
+    List list = data["children"];
+    String content ="";
+    for( var s in list){
+      content +='${s["name"]}  ';
+    }
+    return Card(
+        elevation: 3.0,
+        borderOnForeground: true,
+        child: InkWell(
+          child: Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[Text(data["name"]), Text(content,softWrap: true,)],
+              ),
+              Image(
+                  image: AssetImage("assets/image/img1.png"),
+                  width: 20,
+                  height: 20)
+            ],
+          ),
+          onTap: () {
+            Toast.toast(context, msg: "你点击我了!");
+          },
+        ));
   }
 }
